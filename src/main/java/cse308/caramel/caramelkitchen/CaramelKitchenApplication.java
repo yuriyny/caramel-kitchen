ba690fd006fb6dd96663dd8989633ba90c7f48ba
Yuriy Burshtyko <yuriy.burshtyko@stonybrook.edu>
2019-10-25 20:09:01 -0400

    Initial project setup

---
+++ src/main/java/cse308/caramel/caramelkitchen/CaramelKitchenApplication.java
@@ -0,0 +1,13 @@
+package cse308.caramel.caramelkitchen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaramelKitchenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaramelKitchenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cse308/caramel/caramelkitchen/CaramelKitchenApplicationTests.java
@@ -0,0 +1,13 @@
+package cse308.caramel.caramelkitchen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaramelKitchenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
