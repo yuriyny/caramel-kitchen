6f5af3c5eb291daa2ef5aef7166b97128efa75dc
Teresa Wang <wangteresaliu@gmail.com>
2019-11-27 17:22:10 -0500

    intermediateIngredient now stored in recipe and in gameState.

--- src/main/java/cse308/caramel/caramelkitchen/game/model/GameState.java
+++ src/main/java/cse308/caramel/caramelkitchen/game/model/GameState.java
@@ -1,6 +1,5 @@
 package cse308.caramel.caramelkitchen.game.model;
 
-import cse308.caramel.caramelkitchen.game.persistence.Recipe;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -11,6 +10,6 @@ import java.util.Collection;
 public class GameState {
     String gameId;
     String recipeId;
-    Collection<String> stepsCompleted;
+    Collection<IntermediateIngredient> intermediateIngredients; //This is to make it easier to retrieve if comments are used
     Collection<Double> scores;
 }
